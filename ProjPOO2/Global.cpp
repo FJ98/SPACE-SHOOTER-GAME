5fe97d805e1724fc12f3abe54bb9984d3e057d96
FJ98 <felix.solano@utec.edu.pe>
2019-06-06 21:15:55 -0500

    commit

--- ProjPOO2/Global.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by felix on 5/30/2019.
-//
-
-#include "Global.h"
-
-int Global::highScore = 0;
--- ProjPOO2/cmake-build-debug/CMakeFiles/ProjPOO2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ProjPOO2.
-# This may be replaced when dependencies are built.
